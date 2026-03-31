a160882b9e4f347b76c11f9311176ad60f67399f
Azadhossain288 <azadhossain016288@gmail.com>
2026-03-31 22:42:04 +0600

    Setup react file

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+
+       <h2>Hello</h2>
+       <p>Welcome to our website</p>
+       
+    </div>
+    
+  )
+}
+
+export default App
